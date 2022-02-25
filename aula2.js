1f0cd4c8c238e1c32eab2ff92986c1fa4a16f4fa
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2022-02-25 18:09:42 -0300

    Continuar no Bloco de codigo 1.5.2 da apostila 02

---
+++ aula2.js
@@ -0,0 +1,39 @@
+//const: 
+const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
+
+//arrow function
+//() => {}
+
+//verificar se o nome começa com A
+const resultante = nomes.filter((n) => {
+    return n.startsWith("A")
+});
+
+console.log(resultante)
+
+// void f (int a, float b){
+    
+// }
+// f(1,2)
+
+
+// class Pessoa{
+//     public void andar (){
+        
+//     }
+//     variavel = andar;
+//     //em javascrpt uma função é uma cidada de primeira classe, pode ser atribuida a variavel
+// }
+
+
+
+// v1 = []
+// v1[0] = 3.4
+// v1[10] = 2
+// v1[2] = "abc"
+
+// console.log(v1.length)
+// console.log(v1)
+// for (let i = 0; i < v1.length; i++) {
+//     console.log (v1[i])
+// }
